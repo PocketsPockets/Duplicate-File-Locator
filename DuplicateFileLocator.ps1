148b523b8174d3bf9a6d7dbc2fade647d2858a01
PocketsPockets <65618555+PocketsPockets@users.noreply.github.com>
2021-12-15 01:29:37 -0500

    Add files via upload

---
+++ DuplicateFileLocator.ps1
@@ -0,0 +1,52 @@
+﻿#Variables
+$directoryToTest = "Unassigned"
+$fileInDirectory = "Unassigned"
+$currentFile = "Unassigned"
+$testedFile = "Unassigned"
+$outputDirectory = "Unassigned"
+
+#Directory to look for duplicated files in
+Write-Output "Directory to test: (Add '\' at the end)"
+$directoryToTest = Read-Host
+Write-Output "Testing Directory: "
+Write-Output $directoryToTest
+
+#Directory to save relevant txt files to (make sure to add '\' at the end
+Write-Output "Ouput text files to this directory: (Add '\' at the end of path)"
+$outputDirectory = Read-Host
+Write-Output "Text files will be outputted to: "
+Write-Output $outputDirectory
+
+#Get files in the testing directory
+$filesInDirectory = Get-ChildItem -Recurse -File -Name $directoryToTest
+Write-Output $filesInDirectory
+
+#Output those files to a text file
+$filesInDirectory | Out-File $outputDirectory"\FilesInDirectory.txt"
+
+#Get number of files in directory
+$iterations = (Get-Content $outputDirectory"FilesInDirectory.txt").Length
+Write-Output $iterations
+#Get the hash and check for duplicates
+for ($i = 0; $i -le $iterations; $i++)
+{
+    $currentFile = Get-Content $outputDirectory"\FilesInDirectory.txt" | Select -Index $i
+    $cfHashValue = Get-FileHash $directoryToTest$currentFile
+    Write-Output $cfHashValue" Current;"
+    for ($iT = $i; $iT -le $iterations; $iT++)
+    {
+        $testedFile = Get-Content $outputDirectory"\FilesInDirectory.txt" | Select -Index ($iT)
+        $tfHashValue = Get-FileHash $directoryToTest$testedFile
+        Write-Output $tfHashValue
+        if (($cfHashValue.Hash -eq $tfHashValue.Hash -and ($i -ne $iT)))
+        {
+            $matchFile = ("Original: " + $directoryToTest + $currentFile + "`nDuplicate: " + $directoryToTest + $testedFile)
+            $matchFile | Out-File ($outputDirectory + "Duplucate" + $i + ".txt")
+            Write-Output "Duplicate found!!!!!!!!!!!!!!!!"
+        }
+    }
+}
+
+#clean up
+Remove-Item -Path ($outputDirectory + "FilesInDirectory.txt")
+Write-Output $iterations
